58179f99425537a260a0370844828139f91b3dbc
Ky Nguyen <kynguyen@Kys-MacBook-Pro.local>
2020-05-12 03:31:04 +0700

    Initial Commit

---
+++ LearnAlgo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnAlgo
+//
+//  Created by Ky Nguyen on 5/12/20.
+//  Copyright © 2020 Ky Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
